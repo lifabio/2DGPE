d6d4cad1f7ef552962d28c8f8571424b94eaa093
ut <ut@NBLINGUA>
2017-06-04 19:45:04 -0400

    Add project files.

---
+++ 2DGPE_v1/2DGPE_v1.f90
@@ -0,0 +1,25 @@
+!  $2DGPE_v1.f90 
+!
+!  FUNCTIONS:
+!  $2DGPE_v1 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: $2DGPE_v1
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program $2DGPE_v1
+
+    implicit none
+
+    ! Variables
+
+    ! Body of $2DGPE_v1
+    print *, 'Hello World'
+
+    end program $2DGPE_v1
+
