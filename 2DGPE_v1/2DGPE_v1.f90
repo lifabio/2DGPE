60d89140676f0f5d3a4e289563e8a73e3ec52657
ut <ut@NBLINGUA>
2017-06-04 20:08:46 -0400

    Added the Module dati to store the variables.

--- 2DGPE_v1/2DGPE_v1.f90
+++ 2DGPE_v1/2DGPE_v1.f90
@@ -1,7 +1,7 @@
 !  $2DGPE_v1.f90 
 !
 !  FUNCTIONS:
-!  $2DGPE_v1 - Entry point of console application.
+!  $2DGPE_v1 - 
 !
 
 !****************************************************************************
@@ -11,9 +11,23 @@
 !  PURPOSE:  Entry point for the console application.
 !
 !****************************************************************************
+    Module dati
+        implicit none
+        save
+	    !Double/Single precision definition
+        integer, parameter :: single = selected_real_kind(6,37)
+        integer, parameter :: double = selected_real_kind(15,307)
 
+        !Global const.
+        real(kind=double), parameter    :: pi=3.1415926535897932384626433832795d0
+        complex(kind=double), parameter :: iu=(0.d0,1.d0)   ! imaginary Unit
+
+	    !Global Variables
+
+    endmodule dati
+    
     program $2DGPE_v1
-
+    use dati
     implicit none
 
     ! Variables
